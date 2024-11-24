5cecb22be5763d87c6c57d7dac25a1d20c11c68f
Isaque Estolano de Souza <isaqueestolanossi04@gmail.com>
2024-11-24 12:50:47 -0300

    Atualizar configuração do Sequelize e ajustar o arquivo package.json para refletir mudanças na estrutura do projeto

--- models/dados.js
+++ models/dados.js
@@ -1,14 +1,15 @@
 //Database
     const { Sequelize, DataTypes } = require('sequelize');
-    const sequelize = new Sequelize('postgresql://tutortimedb_owner:aCklVi10tTsv@ep-frosty-mode-a5j3mrx4.us-east-2.aws.neon.tech/tutortimedb?sslmode=require',{
-        dialect:'postgress',
-        logging:false,
+    const sequelize = new Sequelize('postgresql://tutortimedb_owner:aCklVi10tTsv@ep-frosty-mode-a5j3mrx4.us-east-2.aws.neon.tech/tutortimedb?sslmode=require', {
+        dialect: 'postgres',
+        logging: false,
         dialectOptions: {
-            ssl:{
-                require:true,
-                rejectunauthorized:false,
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
             }
-    }})
+        }
+    });
     let contador =0
 //Chalk
     const chalk = require('chalk')
